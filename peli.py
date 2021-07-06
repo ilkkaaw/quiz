059a208deb31ce7c7227af3546e897c955aa304f
gitmikko <mikko.malm87@gmail.com>
2021-07-06 10:23:11 +0300

    tehty muutoksia

--- peli.py
+++ peli.py
@@ -1,16 +1,26 @@
 
-points = 0
+quiz = {
+    1: {"question": "Who was the leader for the group?", "answer" : "rick"},
+    2: {"question": "Who is Carol's best friend?", "answer" : "deryl"},
+    3: {"question": "Who was killed rather dramatically with a baseball bat in the Walking Dead?", "answer" : "glenn"},
+    4: {"question": "Who delivered the deadly blow?", "answer" : "negan"}
+}
+
+score = 0
 print("Welcome to the quiz!\nThis section was made by Krisztina and Mikko :)")
 print()
 
 while True:
-    question1 = input("Who was killed rather dramatically with a baseball bat in the Walking Dead? ")
-    if question1.lower() == "glenn" or question1.lower() == "glenn rhee":
-        points+=1
-    question2 = input("Who delivered the deadly blow? ")
-    if question2.lower() == "negan" or question2.lower() == "negan smith":
-        points+=1
-    
-    break
+    for question in quiz:
+        print(quiz[question]["question"])
+        answer = input("Enter your answer: ")
 
-print("Points: ", points)
+        if answer.lower() == quiz[question]["answer"]:
+            score += 1
+
+    print("Score:", score)
+    print()
+    again = input("Do you want to play again? (Y to play again, N to quit) ")
+
+    if again == "N":
+        break
