df01264aeb0f2187d9ab902e3b405c2d8c00680d
ilkka.aw <ilkka.pekkala@awacademyconsultant.fi>
2021-07-06 12:46:51 +0300

    "Added my suggestions and comments"

--- peli.py
+++ peli.py
@@ -1,25 +1,28 @@
 quiz = {
     1: {"question": "Who was the leader for the group?", "answer" : "rick"},
-    2: {"question": "Who is Carol's best friend?", "answer" : "deryl"},
-    3: {"question": "Who was killed rather dramatically with a baseball bat in the Walking Dead?", "answer" : "glenn"},
+    2: {"question": "Who is Carol's best friend?", "answer" : "daryl"}, #dAryl, ei deryl
+    3: {"question": "Who was killed rather dramatically with a baseball bat in the Walking Dead?", "answer" : "glenn"}, #Also Abraham, but multiple possibilties in a dictionary is hard
     4: {"question": "Who delivered the deadly blow?", "answer" : "negan"}
 }
+total_questions = len(quiz)
 
 score = 0
 print("Welcome to the quiz!\nThis section was made by Krisztina and Mikko :)")
 print()
 
 while True:
-    for question in quiz:
+    for question in quiz: #This was actually a good way to go through them all!
         print(quiz[question]["question"])
         answer = input("Enter your answer: ")
 
         if answer.lower() == quiz[question]["answer"]:
+            print("Correct!") #Tell the player when it goes right.
             score += 1
-
-    print("Score:", score)
+        else:
+            print("Wrong") #Tell the player when it goes wrong.
+    print(f"Score: {score} out of {total_questions}") #Score means nothing if you don't know what is the maximum.
     print()
     again = input("Do you want to play again? (Y to play again, N to quit) ")
 
-    if again[0].lower() == "n":
+    if again[0].lower() == "n": #This only checks for a negative answer. Any other answer means yes!
         break
